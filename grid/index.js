76fe12c4167159dafc4349e3cc543c555051e45a
Muneeb <MuneebRabaney@users.noreply.github.com>
2018-04-26 12:10:23 +0200

    Add root export for imported files

---
+++ grid/index.js
@@ -0,0 +1,11 @@
+import Row from './row'
+import Column from './column'
+import Container from './container'
+import Responsive from './responsive'
+
+export {
+  Row,
+  Column,
+  Container,
+  Responsive,
+}
